4001553ee5392ef1561e80c9a0ffe80aee2ca01f
Joanne Greenhalgh <joanne.greenhalgh@roq.co.uk>
2023-02-21 14:32:34 +0000

    Added further unit tests

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -12,4 +12,29 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	
+	@Test
+	public void testFeeding() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.feeds();
+		assertEquals(cat.howHungry(), 30);
+	}
+	
+	@Test
+	public void testRuns() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.runs();
+		assertEquals(cat.howHungry(), 80);
+	}
+	
+	@Test
+	public void testCatDies() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.runs();
+		cat.runs();
+		assertTrue(!cat.isAlive());
+	}
 }
