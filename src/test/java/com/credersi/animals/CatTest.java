c21e070a524807eefa20b5d3c05ed615e42c436f
Joanne Greenhalgh <joanne.greenhalgh@roq.co.uk>
2023-02-21 14:39:56 +0000

    Added further tests

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -14,25 +14,39 @@ public class CatTest {
 	}
 	
 	@Test
-	public void testFeeding() {
+	public void testFeedingOnce() {
 		Cat cat = new Cat();
-		assertTrue(cat.isAlive());
 		cat.feeds();
 		assertEquals(cat.howHungry(), 30);
 	}
 	
 	@Test
+	public void testFeedingUntilNotHungry() {
+		Cat cat = new Cat();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 0);
+	}
+	
+	@Test
 	public void testRuns() {
 		Cat cat = new Cat();
-		assertTrue(cat.isAlive());
 		cat.runs();
 		assertEquals(cat.howHungry(), 80);
 	}
 	
 	@Test
+	public void testFeedAndRun() {
+		Cat cat = new Cat();
+		cat.runs();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 60);
+	}
+	
+	@Test
 	public void testCatDies() {
 		Cat cat = new Cat();
-		assertTrue(cat.isAlive());
 		cat.runs();
 		cat.runs();
 		assertTrue(!cat.isAlive());
